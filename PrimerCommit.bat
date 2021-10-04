bc29dcaf50f422fa7e00dd1a84d892da1aa6ce19
gatriana <gatriana3000@gmail.com>
2021-10-04 16:28:45 -0500

    Commit FrontEnd Persona * Gaston

--- PrimerCommit.bat
+++ PrimerCommit.bat
@@ -3,7 +3,7 @@ git status
 git add .
 git status
 git commit -m "Commit FrontEnd Persona * Gaston"
-git branch -M main
+git branch -M gaston
 git remote add origin https://github.com/Jairo2021/Agendamiento.git
-git push -u origin main
+git push -u origin gaston
 git status
